85f4bc00504b873412e6ea561268b6538fa6a538
freedomlang <blackywkl@gmail.com>
2019-01-06 14:35:27 +0800

    chore: add test script

---
+++ src/assets/js/__test__/index.spec.js
@@ -0,0 +1,12 @@
+import { expect } from "chai";
+import idGenerator from "../randomID.js";
+
+describe("Random id", () => {
+  it("should generate random ids", () => {
+    function hasDuplicates(array) {
+      return new Set(array).size !== array.length;
+    }
+    const ids = [...Array(10)].map(() => idGenerator());
+    expect(hasDuplicates(ids)).to.equal(false);
+  });
+});
--- src/assets/js/randomID.js
+++ src/assets/js/randomID.js
@@ -1,8 +1,19 @@
+function randomFourChar() {
+  return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+}
+
 function idGenerator() {
-    var S4 = function() {
-       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-    };
-    return ('_'+S4()+S4()+S4()+S4()+S4()+S4()+S4()+S4());
+  return (
+    "_" +
+    randomFourChar() +
+    randomFourChar() +
+    randomFourChar() +
+    randomFourChar() +
+    randomFourChar() +
+    randomFourChar() +
+    randomFourChar() +
+    randomFourChar()
+  );
 }
 
 module.exports = idGenerator;
